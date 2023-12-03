141ca2b16d2adecda8b2463801874bbe32d5beeb
coder2800 <devanshagarwal100@gmail.com>
2023-12-04 01:17:02 +0530

    changed the about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,8 @@ import noteContext from '../context/notes/noteContext';
 export default function About() {
   const a = useContext(noteContext);
   return (
-    <div className='mx-2 my-1'>
-      This is about {a.name} from {a.class}
+    <div className='mx-2 my-3' style={{"textAlign": "center", "fontSize" : "5vh"}}>
+      Hello, Skyscroll is a safe and secure place to keep your notes. We assure you that your email, passwords and notes can never be accessed by anyone else not even by us.
     </div>
   )
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,20 +2,21 @@ import React from 'react'
 import userContext from '../context/user/userContext'
 
 const SignUp = () => {
+  
   return (
     <div className='my-3'>
       <form style={{"display": "flex", "flexDirection": "column"}}>
         <div className="form-group my-3">
-          <label htmlFor="exampleInputEmail1">Email address</label>
+          <label htmlFor="email">Email address</label>
           <input type="email" name="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"/>
         </div>
         <div className="form-group my-3">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="password" placeholder="Enter your Password"/>
+          <label htmlFor="password">Password</label>
+          <input type="password" name='password' className="form-control" id="password" placeholder="Enter your Password"/>
         </div>
         <div className="form-group my-3">
-          <label htmlFor="exampleInputPassword1">Re-enter Password</label>
-          <input type="password" className="form-control" id="cpassword" placeholder="Re-enter Password"/>
+          <label htmlFor="cpassword">Re-enter Password</label>
+          <input type="password" name='cpassword' className="form-control" id="cpassword" placeholder="Re-enter Password"/>
         </div>
         <button type="submit" className="btn btn-primary my-3">Submit</button>
       </form>
